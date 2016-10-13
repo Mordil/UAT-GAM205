73e610a94bd0ebf2a9428457fec6fd8d59a53064
Nathan <nathanh@l4digital.com>
2016-10-12 23:26:34 -0700

    Added good/bad color materials for tanks, added tank prefab, created motor script, and tank settings script.

---
+++ NHarris_UATanks/Assets/Scripts/Tank/TankMotor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class TankMotor : MonoBehaviour
+{
+    // references to the gameobject containers' transforms for the tank body segments
+    [SerializeField]
+    private Transform _bodyTransform;
+
+    [SerializeField]
+    private CharacterController _characterController;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        if (_characterController == null)
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        if (_bodyTransform == null)
+        {
+            _bodyTransform = GetComponent<Transform>();
+        }
+	}
+    
+    /// <summary>
+    /// Moves the tank forward by the the speed provided.
+    /// </summary>
+    /// <param name="speed">Meters per second to move by.</param>
+    public void Move(float speed)
+    {
+        // Move forward from the current rotation / position
+        Vector3 speedVector = transform.forward * speed;
+
+        // simpleMove applies Time.deltaTime, so it's unneeded by us <-- DON'T FORGET
+        _characterController.SimpleMove(speedVector);
+    }
+
+    /// <summary>
+    /// Rotates a the tank by the speed provided.
+    /// </summary>
+    /// <param name="speed">The speed in meters per second to rotate by.</param>
+    public void Rotate(float speed)
+    {
+        // Maintain current "up" position, to rotate horizontally.
+        Vector3 rotateVector = Vector3.up * speed;
+        rotateVector *= Time.deltaTime;
+
+        // apply the rotation
+        _bodyTransform.Rotate(rotateVector, Space.Self);
+    }
+}
---
+++ NHarris_UATanks/Assets/Scripts/Tank/TankSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TankSettings : MonoBehaviour
+{
+    public float BodyRotationSpeed { get { return _bodyRotationSpeed; } }
+    public float MovementSpeed { get { return _movementSpeed; } }
+
+    [SerializeField]
+    [Tooltip("Units (in meters) to move per second.")]
+    private float _movementSpeed = 3f;
+
+    [Tooltip("Rotation in degrees the body should rotate.")]
+    [SerializeField]
+    private float _bodyRotationSpeed = 180f;
+}
