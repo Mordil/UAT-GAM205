e651fc69537277c7c864e2581790a76c50fb5688
Nathan <nathanh@l4digital.com>
2016-11-05 00:46:29 -0700

    Added TutorialTank assets; added audio to many prefabs and scripts;

---
+++ NHarris_UATanks/Assets/Scripts/Audio/AudioSourceManager.cs
@@ -0,0 +1,22 @@
+﻿using L4.Unity.Common.Audio;
+using UnityEngine;
+
+public class AudioSourceManager : AudioSourceExtenderBase
+{
+    protected override void Start()
+    {
+        base.Start();
+
+        AudioSource.volume = GameManager.Instance.Settings.GetAudioLevelFromChannel(AudioChannel);
+        GameManager.Instance.Settings.ChannelVolumeChanged += OnVolumeSourceChanged;
+    }
+
+    protected override void OnVolumeSourceChanged(object sender, ChannelVolumeChangedEventArgs e)
+    {
+        if (e.Channel == AudioChannel.Master ||
+            e.Channel == AudioChannel)
+        {
+            AudioSource.volume = GameManager.Instance.Settings.GetAudioLevelFromChannel(AudioChannel);
+        }
+    }
+}
