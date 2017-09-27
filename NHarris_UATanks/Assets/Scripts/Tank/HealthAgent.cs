03ad12913f706d3f58f480b29ff9c525fa4579ef
Nathan Harris <2nd.lt.harris@gmail.com>
2017-09-26 22:35:33 -0700

    Add Kill Value & Messaging on Bullet Collision to HealthAgent

--- NHarris_UATanks/Assets/Scripts/Tank/HealthAgent.cs
+++ NHarris_UATanks/Assets/Scripts/Tank/HealthAgent.cs
@@ -4,11 +4,15 @@ using UnityEngine.Events;
 public class HealthAgent : MonoBehaviour, ITankComponent
 {
     public UnityEvent<int> OnTookDamage;
+    public UnityEvent<int> OnGainedHealth;
+    public UnityEvent<int> OnMaxHealthChanged;
+    public UnityEvent OnKilled;
 
     [SerializeField]
     [ReadOnly]
     private int _currentHealth;
     private int _maxHealth;
+    private int _killValue;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -22,17 +26,34 @@ public class HealthAgent : MonoBehaviour, ITankComponent
     public void SetUp(TankSettings settings)
     {
         _maxHealth = settings.MaxHealth;
+        _currentHealth = _maxHealth;
+        _killValue = settings.KillValue;
+    }
+
+    public void ModifyMaxHealth(int amount)
+    {
+        _maxHealth += amount;
+        OnMaxHealthChanged.Invoke(_maxHealth);
     }
 
     public void AddHealth(int amount)
     {
         _currentHealth = Mathf.Clamp(_currentHealth + amount, 0, _maxHealth);
+        OnGainedHealth.Invoke(_currentHealth);
     }
 
     public void TakeDamage(int amount)
     {
         _currentHealth -= amount;
-        OnTookDamage.Invoke(_currentHealth);
+
+        if (_currentHealth <= 0)
+        {
+            OnKilled.Invoke();
+        }
+        else
+        {
+            OnTookDamage.Invoke(_currentHealth);
+        }
     }
 
     private void OnBulletCollision(TankBulletAgent agent)
@@ -42,6 +63,11 @@ public class HealthAgent : MonoBehaviour, ITankComponent
             agent.Owner.gameObject.layer != this.gameObject.layer)
         {
             TakeDamage(agent.Damage);
+
+            if (_currentHealth <= 0)
+            {
+                agent.Owner.SendMessage(BaseTankManager.KILLED_TARGET_MESSAGE, _killValue, SendMessageOptions.DontRequireReceiver);
+            }
         }
     }
 }
